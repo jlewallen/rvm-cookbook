e7f94c9011562558150d55ce19c50af4ea53541a
Jacob Lewallen <jlewallen@gmail.com>
2011-08-28 09:58:45 -0700

    Following redirections when getting installation script.

--- definitions/install_rvm.rb
+++ definitions/install_rvm.rb
@@ -1,7 +1,7 @@
 define :install_rvm do
   bash "install_rvm" do
     code <<-EOH
-    bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+    bash < <(curl -L -s https://rvm.beginrescueend.com/install/rvm)
     EOH
     not_if { File.exists? "/usr/local/rvm" }
   end
